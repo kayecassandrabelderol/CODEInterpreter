6c33301277910ca5e678692b8ef687b4571249c7
Kaye Cassandra Belderol <kayecassandra.belderol@cit.edu>
2023-04-20 22:15:40 +0800

    Modify program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using CODEInterpreter_v1;
+using CODEInterpreter_v1.Utils;
+using static CODEInterpreter_v1.Utils.CODEParser;
+
+var fileName = "C:\\Belderol\\3rd Year\\Second Semester\\CS322 Programming Languages\\Activities\\CODEInterpreter\\Utils\\test.code";
+var fileContents = File.ReadAllText(fileName);
+var inputStream = new AntlrInputStream(fileContents);
+var codeLexer = new CODELexer(inputStream);
+var commonTokenStream = new CommonTokenStream(codeLexer);
+var codeParser = new CODEParser(commonTokenStream);
+var codeContext = codeParser.program();
+var visitor = new CODEVisitor();
+visitor.Visit(codeContext);
